cb852821b426eab7b4a66b6c5271568fea900270
Shaun Prince <shaun@techfusion.ca>
2026-01-29 15:30:47 -0800

    fix: Update SHA256 after version bump to 7.1.1

--- Formula/neofetch.rb
+++ Formula/neofetch.rb
@@ -5,18 +5,14 @@ class Neofetch < Formula
   desc "Fast, highly customizable system info script"
   homepage "https://github.com/Suparious/neofetch"
   url "https://github.com/Suparious/neofetch/archive/refs/tags/7.1.1.tar.gz"
-  sha256 "8bbd607438b093049c0a0c1cdec6e49050a91e73f8ea0b3e00c0a743672bd6d2"
+  sha256 "62b4a7ad69ee31d8b1a1b64b23ea34ab2c8d3c6c560ac7bd35422a463af68169"
   license "MIT"
   head "https://github.com/Suparious/neofetch.git", branch: "master"
-
-  # Neofetch is a bash script with no compiled dependencies
-  # It works with bash 3.2+ which is included with macOS
 
   def install
     inreplace "neofetch", "/usr/share", "#{prefix}/share"
     bin.install "neofetch"
     man1.install "neofetch.1"
-    (share/"neofetch").install Dir["ascii/*"]
   end
 
   test do
